d7212e3b957c420788215f5b8cd97df580464fe2
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-21 00:02:22 -0300

    Working with FileStream and StreamReader

---
+++ FileStreamAndStreamReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamAndStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+                
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                if (sr != null) sr.Close(); //Necessário fechar o file e stream reader pois o C# não fecha sozinho ele
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
